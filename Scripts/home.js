b67e88d00400881538541bfe1b3c8d7691ad1d10
Luiz Camargo <luiz.zito@hotmail.com>
2022-03-07 12:54:55 -0300

    first commit

---
+++ Scripts/home.js
@@ -0,0 +1,42 @@
+
+
+function toggleMenu() {
+    tratarMenu();
+    tratarLinhasMenu(); 
+}
+
+function tratarMenu () {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');    
+}
+
+function tratarLinhasMenu () {
+    const linhas = document.getElementsByClassName('linha');  
+    for (let index = 0; index < linhas.length; index++) {
+        linhas[index].classList.toggle('active');    
+    }
+}
+
+function openResume() {
+    criarLink('../Assets/CV English - Luiz Camargo [UPDATE] (1).pdf');
+  }
+
+function verGithub() {
+    criarLink('https://github.com/luizenrique99');
+}
+
+function verLinkedin() {
+    criarLink('https://www.linkedin.com/in/luiz-enrique-nobrega-de-camargo-2bb849141/');
+}
+  
+function criarLink(url) {
+    const tagA = document.createElement("a");    
+    tagA.href = url;
+    tagA.target = 'blank';
+    tagA.click();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const btnMobile = document.getElementById('btn-mobile');
+    btnMobile.addEventListener('click', toggleMenu);
+});
